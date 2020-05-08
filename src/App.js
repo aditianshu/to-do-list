d62e55ee7342e5004b6331eee914596d35c165cc
Aditi Anshu <55697933+aditianshu@users.noreply.github.com>
2020-05-08 21:16:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './List'
 
 function App() {
   return (
     <div className="App">
-      <TaskList/>
+      <List/>
     </div>
   );
 }
